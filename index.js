b9b155e2a290d87f33e888d7ab2ff2ff5327eb54
ayubdev <ayubgardy898@gmail.com>
2022-08-05 10:20:44 +0300

    Done the assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrStr, eventName) {
+    const messages =[];
+    for(let i = 0; i < arrStr.length; i++) {
+        messages.push(`Thank you, ${arrStr[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
